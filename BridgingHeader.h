bc8d99d26125646c89ce1a85aa63eb9a4242bd03
Florian Marcu <florian.mrc@gmail.com>
2015-05-26 23:08:39 +0100

    Implemented the Chat, bitch!

--- BridgingHeader.h
+++ BridgingHeader.h
@@ -11,6 +11,22 @@
 
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <Firebase/Firebase.h>
+
+#import "JSQMessagesViewController.h"
+#import "JSQMessageData.h"
+#import "JSQMessagesBubbleImageFactory.h"
+#import "JSQMessagesAvatarFactory.h"
+#import "JSQSystemSoundPlayer.h"
+#import "UIView+JSQMessages.h"
+#import "JSQSystemSoundPlayer+JSQMessages.h"
+#import "UIColor+JSQMessages.h"
+#import "JSQMessagesCollectionViewCell.h"
+#import "JSQMessagesCollectionView.h"
+#import "JSQMessagesInputToolbar.h"
+#import "JSQMessagesToolbarContentView.h"
+#import "JSQMessagesComposerTextView.h"
+
 @import AVFoundation;
 
 #endif
